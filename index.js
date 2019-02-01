0d54e65364450260d5e5112d7e0ad850b8a9abb0
Zerojame <toypoyomn03@outlook.com>
2019-02-01 17:25:24 +0700

    [edited] edit port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
     res.send('Hello World!!')
 })
 
-app.set('port', 3000)
+app.set('port', (process.env.PORT || 4000))
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
