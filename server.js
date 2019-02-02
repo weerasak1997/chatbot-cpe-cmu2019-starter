49c533fcef1a4b3b070eb8f1c779045ef343deb1
590610661 weerasak chiangsoi <jame25193@gmail.com>
2019-02-02 14:37:48 +0700

    a

--- server.js
+++ server.js
@@ -20,20 +20,46 @@ app.post('/webhook', middleware(config), (req, res) => {
     if (event.type === 'message') {
       const message = event.message;
       console.log(message)
-      if(message.type === 'audio'){
-        console.log('audio');
-        
-        client.replyMessage(event.replyToken, {
-          type: 'text',
-          text: 'wtf'
+      client.replyMessage(event.replyToken, 
+        {
+          "type": "template",
+          "altText": "This is a buttons template",
+          "template": {
+              "type": "buttons",
+              "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "Menu",
+              "text": "Please select",
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com/"
+              },
+              "actions": [
+                  {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=123"
+                  },
+                  {
+                    "type": "message",
+                    "label": "Add to cart",
+                    "text": "no no no"
+                  },
+                  {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://google.com"
+                  }
+              ]
+          }
         })
-      } else{
-        console.log('non audio');
-      client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: message.type
-      })
     }
+  })
+  
+    
     }
 
 })
