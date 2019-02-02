0c53de866a4eb2c47183ed5da9fefef7760aab75
590610661 weerasak chiangsoi <jame25193@gmail.com>
2019-02-02 13:34:34 +0700

    a

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const config = {
   channelAccessToken: 'c9HRUFxkCRt5VIknZRPxWKDzmznAtiHzImOqBPpc5OjwvJgwAEKh5yD/CJ1oXGcN3LvqxeosYrhwQ2Rqbs5Yprmqyp4VkFFyi1w5A4i8grZ7dDRlXn/K83hXKgG99+YhGg1I+UJaH4rqTn9hTovCygdB04t89/1O/w1cDnyilFU=',
   channelSecret: '5e506ff69e67d6114a9e736109697d58'
 }
-const event =new ClientRect(config)
+const event =new Client(config)
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
     
 })
 app.post('/webhook', middleware(config), (req, res) => {
-  // res.send('Hello World!!')
+   res.send('Hello World!!')
   const event = req.body.events[0];
     if (event.type === 'message') {
       const message = event.message;
